b55a8ce8052ded0c7204d1e831904ffa6324151d
KashanAltaf <kashanaltaf137@gmail.com>
2023-12-02 17:55:33 +0500

    Project

--- app/src/main/java/com/ass3/i200547_i202433_project/MainActivity6_Home.java
+++ app/src/main/java/com/ass3/i200547_i202433_project/MainActivity6_Home.java
@@ -3,8 +3,12 @@ package com.ass3.i200547_i202433_project;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
@@ -14,13 +18,75 @@ import java.util.Locale;
 public class MainActivity6_Home extends AppCompatActivity {
     CalendarView calendarView;
     Calendar calendar;
-
+    ImageButton i1, i2, i3, i4 ,i5, i6;
+    Button b1, b2, b3, b4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
         calendarView = (CalendarView) findViewById(R.id.calendarview);
+        i1 = (ImageButton) findViewById(R.id.z21) ;
+        i2 = (ImageButton) findViewById(R.id.z23);
+        i3 = (ImageButton) findViewById(R.id.z24);
+        i4 = (ImageButton) findViewById(R.id.z26);
+        i5 = (ImageButton) findViewById(R.id.z27);
+        b1 = (Button) findViewById(R.id.z8);
+        b2 = (Button) findViewById(R.id.z11);
+        b3 = (Button) findViewById(R.id.z14);
+        b4 = (Button) findViewById(R.id.z17);
+        i6 = (ImageButton) findViewById(R.id.z90);
         calendar = Calendar.getInstance();
+
+        i1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity1(); }
+        });
+
+        i2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity1(); }
+        });
+
+        i3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity1(); }
+        });
+
+        i4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity1(); }
+        });
+
+        i5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity1(); }
+        });
+        i6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Activity3();
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity1(); }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity1(); }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity1(); }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity2(); }
+        });
 
         setDate(2, 12, 2023);
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
@@ -43,5 +109,18 @@ public class MainActivity6_Home extends AppCompatActivity {
         calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
         long milli = calendar.getTimeInMillis();
         calendarView.setDate(milli);
+    }
+
+    public void Activity1(){
+        Intent intent = new Intent(this, MainActivity6_Home.class);
+        startActivity(intent);
+    }
+    public void Activity2(){
+        Intent intent = new Intent(this, MainActivity7_Exercise.class);
+        startActivity(intent);
+    }
+    public void Activity3(){
+        Intent intent = new Intent(this, MainActivity5_Settings.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/ass3/i200547_i202433_project/MainActivity7_Exercise.java
+++ app/src/main/java/com/ass3/i200547_i202433_project/MainActivity7_Exercise.java
@@ -2,13 +2,52 @@ package com.ass3.i200547_i202433_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity7_Exercise extends AppCompatActivity {
-
+    ImageButton i1, i2, i3, i4, i5, i6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_exercise);
+        i1 = (ImageButton) findViewById(R.id.zz41);
+        i2 = (ImageButton) findViewById(R.id.zz34);
+        i3 = (ImageButton) findViewById(R.id.zz36);
+        i4 = (ImageButton) findViewById(R.id.zz37);
+        i5 = (ImageButton) findViewById(R.id.zz39);
+        i6 = (ImageButton) findViewById(R.id.zz40);
+
+        i1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity1(); }
+        });
+        i2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity2(); }
+        });
+        i3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity2(); }
+        });
+        i4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity2(); }
+        });
+        i5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Activity2(); }
+        });
+    }
+    public void Activity1(){
+        Intent intent = new Intent(this, MainActivity5_Settings.class);
+        startActivity(intent);
+    }
+    public void Activity2(){
+        Intent intent = new Intent(this, MainActivity6_Home.class);
+        startActivity(intent);
     }
 }
