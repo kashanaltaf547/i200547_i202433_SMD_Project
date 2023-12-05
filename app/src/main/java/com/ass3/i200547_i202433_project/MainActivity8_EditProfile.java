1572b97a51cfffc1527ad0263b6242ca26a385cd
KashanAltaf <kashanaltaf137@gmail.com>
2023-12-05 18:48:41 +0500

    Project

--- app/src/main/java/com/ass3/i200547_i202433_project/MainActivity8_EditProfile.java
+++ app/src/main/java/com/ass3/i200547_i202433_project/MainActivity8_EditProfile.java
@@ -1,9 +1,12 @@
 package com.ass3.i200547_i202433_project;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,12 +16,20 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 public class MainActivity8_EditProfile extends AppCompatActivity {
     ImageView i1;
@@ -27,6 +38,8 @@ public class MainActivity8_EditProfile extends AppCompatActivity {
     Button b1;
     String username, email, pass, dob;
     DatabaseReference reference;
+    Uri ImageUri;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +57,13 @@ public class MainActivity8_EditProfile extends AppCompatActivity {
         reference = FirebaseDatabase.getInstance().getReference();
 
         showData();
+
+        l1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePic();
+            }
+        });
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,6 +128,52 @@ public class MainActivity8_EditProfile extends AppCompatActivity {
         e2.setText(email);
         e3.setText(pass);
         e4.setText(dob);
+    }
+    public void choosePic() {
+        Intent intent = new Intent();
+        intent.setType("/image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data!=null && data.getData() != null){
+            ImageUri = data.getData();
+            l1.setImageURI(ImageUri);
+            uploadPic();
+        }
+    }
+    public void uploadPic(){
 
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setTitle("Uploading Image...");
+        pd.show();
+
+        final String randomKey = UUID.randomUUID().toString();
+        StorageReference riversRef = storageReference.child("images/" + randomKey);
+
+        riversRef.putFile(ImageUri).
+                addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        pd.dismiss();
+                        Snackbar.make(findViewById(android.R.id.content), "Image Uploaded", Snackbar.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        pd.dismiss();
+                        Toast.makeText(getApplicationContext(), "Failed to upload", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        double progressPercent = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                        pd.setMessage("Percentage" + (int) progressPercent + "%");
+                    }
+                });
     }
 }
